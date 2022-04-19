459ee3c19060c4ce533751250d40e64bc28f3a61
ArerehRaj <raj.jr@somaiya.edu>
2022-04-19 10:47:48 +0530

    Home screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,36 +3,142 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mental_health_tracker_app/screens/quiz/quiz_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../constants.dart';
+import '../models/tab_icon_list.dart';
+import '../widgets/bottom_bar.dart';
+import './training_screen.dart';
+import 'my_diary_screen.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   static const routeName = '/home-screen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  AnimationController? animationController;
+
+  List<TabIconData> tabIconsList = TabIconData.tabIconsList;
+
+  Widget tabBody = Container(
+    color: FitnessAppTheme.background,
+  );
+
+  @override
+  void initState() {
+    tabIconsList.forEach((TabIconData tab) {
+      tab.isSelected = false;
+    });
+    tabIconsList[0].isSelected = true;
+
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+    tabBody = MyDiaryScreen(animationController: animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+
+  Widget bottomBar() {
+    return Column(
+      children: <Widget>[
+        const Expanded(
+          child: SizedBox(),
+        ),
+        BottomBarView(
+          tabIconsList: tabIconsList,
+          addClick: () {},
+          changeIndex: (int index) {
+            if (index == 0 || index == 2) {
+              animationController?.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  tabBody =
+                      MyDiaryScreen(animationController: animationController);
+                });
+              });
+            } else if (index == 1 || index == 3) {
+              animationController?.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  tabBody =
+                      TrainingScreen(animationController: animationController);
+                });
+              });
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            FlatButton(
-              onPressed: () {
-                print('fsd');
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Home Screen'),
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(QuizScreen.routeName);
-              },
-              child: const Text('Quiz Screen'),
-            ),
-          ],
+    return Container(
+      color: FitnessAppTheme.background,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: FutureBuilder<bool>(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox();
+            } else {
+              return Stack(
+                children: <Widget>[
+                  tabBody,
+                  bottomBar(),
+                ],
+              );
+            }
+          },
         ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Home Screen'),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         children: [
+  //           FlatButton(
+  //             onPressed: () {
+  //               print('fsd');
+  //               FirebaseAuth.instance.signOut();
+  //             },
+  //             child: const Text('Home Screen'),
+  //           ),
+  //           FlatButton(
+  //             onPressed: () {
+  //               Navigator.of(context).pushNamed(QuizScreen.routeName);
+  //             },
+  //             child: const Text('Quiz Screen'),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
 }
